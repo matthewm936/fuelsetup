7be36ea0d6d046aa5b09931afce1711adf802644
Matthew Maloney <98236645+matthewm936@users.noreply.github.com>
2023-03-31 13:40:07 -0600

    Update colors.php

--- views/welcome/colors.php
+++ views/welcome/colors.php
@@ -1,10 +1,22 @@
-<!DOCTYPE php>
-<h1>color.php</h1>
-
-<form action="" method="get">
-        <input type="text" name=size placeholder="input size of table">
-     
-        <input type="text" name=colors placeholder="input size of colors">
-        <button type="submit">Search</button>
+<!DOCTYPE PHP>
+<html lang='en'>
+<head>
+    <title id='print'>Company Name Colors</title>
+</head>
+<body>
+    <br>
+    <br>
+    <form action="<?php echo Uri::create(); ?>" method="get">
+        <input type="text" name=size placeholder="Input size of table">
+        <input type="text" name=colors placeholder="Input size of colors">
+        <button type="submit">Search</button> 
+        <br>
     </form>
-
+    <nav>
+        <a href='?page=gray&
+            colors=<?php if(isset($_GET['colors'])){echo $_GET['colors'];}?>&
+            size=<?php if(isset($_GET['size'])){echo $_GET['size'];}?>'>
+            PrintView</a>
+    </nav>
+</body>
+</html>
