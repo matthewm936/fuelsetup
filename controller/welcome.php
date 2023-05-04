8c9c3d335067317b51c22dede2877ad10b0e6841
JHatch18 <111807211+JHatch18@users.noreply.github.com>
2023-05-04 13:27:58 -0600

    new known bug

--- controller/welcome.php
+++ controller/welcome.php
@@ -24,6 +24,7 @@
 // 1) Can't color when the color table's size is 1
 // 2) The name in the dropdown is not the same name as the color
 // 3) only the first 2 rows in the color table automatically update which cells are colored
+// 4) Colors wit spaces in the name don't work properly
 		
 class Controller_Welcome extends Controller_Template
 {
